52be93b5551818a96e5c483ef3777a6fa099e8e0
Jun0613-spec <kumo5110@gmail.com>
2023-02-17 11:27:02 +0000

    social media app with mysql

---
+++ client/src/components/stories/Stories.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import "./stories.scss";
+import { AuthContext } from "../../context/authContext";
+
+const Stories = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  return (
+    <div className="stories">
+      <div className="story">
+        <img src={currentUser.profilePic} alt="" />
+        <span>{currentUser.name}</span>
+        <button>+</button>
+      </div>
+    </div>
+  );
+};
+
+export default Stories;
