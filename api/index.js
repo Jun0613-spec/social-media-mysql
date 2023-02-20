15b3724560e4e833fc74afeae69803aeb6183cb0
Jun0613-spec <kumo5110@gmail.com>
2023-02-20 09:41:55 +0000

    changed

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "http://https://social-media-front-xi74.onrender.com",
+      "https://social-media-front-xi74.onrender.com",
     ],
   })
 );
